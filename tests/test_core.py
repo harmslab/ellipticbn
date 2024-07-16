50128a69b6e3727a431593293945dbd09c6e6b3f
Michael Harms <harmsm@protonmail.com>
2024-07-15 17:45:56 -0700

    creating basic tests and github workflows

---
+++ tests/test_core.py
@@ -0,0 +1,17 @@
+
+from ElliptiCBn.core import _order_nodes
+
+def test__order_nodes():
+    pass
+
+def test_calc_ellipticity():
+    pass
+
+def test_get_macrocycles():
+    pass
+
+def test_get_ellipticity():
+    pass
+
+def test_plot_results():
+    pass
---
+++ tests/test_run_all.py
@@ -0,0 +1,7 @@
+import ElliptiCBn
+
+def test__file_check():
+    pass
+
+def test_run_all():
+    pass
